e34fdda5fd36450e1112bcce20e8a34bdd49d41e
xdefagot <ximedefagot@gmail.com>
2026-04-21 20:34:30 -0300

    Update PruebaContext provider value

--- Parcial2PROWEBContrera,Defagot,Díaz/parcial2pw/my-parcial2-app/src/pages/Home.jsx
+++ Parcial2PROWEBContrera,Defagot,Díaz/parcial2pw/my-parcial2-app/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { PruebaContext} from "../Context/PruebaContext";
 import "../assets/styles.css/Home.css";
 
 const Home = () => {
-  const contextoCompartido = useContext(PruebaContext);
+  
   
   return (
     <section className="section-home">
@@ -11,7 +11,6 @@ const Home = () => {
         <div className="row justify-content-center">
           <div className="col-md-8">
             <h1 className="">💮Bienvenido a MODA CIRCULAR💮.</h1>
-            <p className="">Valor de contexto: {contextoCompartido}</p>
             <a href="/prendas" className="btn btn-outline-dark mt-3">Ir a la tienda</a>
           </div>
         </div>
